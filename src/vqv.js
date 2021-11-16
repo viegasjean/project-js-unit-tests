326f23352ca5244ebce4f1692bcde13462a9891e
Jean Viegas <jeanmelg@gmail.com>
2021-11-16 16:48:48 -0300

    implementa os testes para função productDetails e arruma vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -21,7 +21,9 @@ const vqv = (nome, idade) => {
     return undefined;
   }
 
-  const frase = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  const frase =
+    `Oi, meu nome é ${nome}!\n` +
+    `Tenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 
   return frase;
 };
